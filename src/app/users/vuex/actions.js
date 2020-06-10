4ba4419f0724e83de21c5744d8694137f552cc0a
Vitezslav Slavik <vitezslavik@gmail.com>
2020-06-10 02:17:00 +0200

    add user request to api

--- src/app/users/vuex/actions.js
+++ src/app/users/vuex/actions.js
@@ -5,6 +5,8 @@ import {
 } from './mutation-types';
 // import axios from "axios";
 import { v4 as uuidv4 } from 'uuid';
+import axios from "axios";
+import {requestConfig} from "../../../main";
 
 
 
@@ -15,15 +17,41 @@ import { v4 as uuidv4 } from 'uuid';
  */
 
 
-export function registerUser({commit}, userobject) {
+export async function registerUser({commit}, userobject) {
     const ID = uuidv4();
     let user = Object.assign({ id: ID }, userobject);
     delete user.password2
-    commit(REGISTER_USER, user);
+
+    // Request Object
+    const requestBody = JSON.stringify({
+        requestData: user
+    })
+
+    // Send request
+    const response = await axios.post('api/users/create_user.php', requestBody, requestConfig)
+    if (response.data && response.data.response !== 'duplicated'){
+        commit(REGISTER_USER, user);
+        return {response:'success'}
+    } else {
+        return response.data
+    }
+
 }
 
-export function loginUser({commit}, user) {
-    commit(LOGIN_USER, user);
+export async function loginUser({commit}, user) {
+
+
+    // Request Object
+    const requestBody = JSON.stringify({
+        requestData: user
+    })
+    console.log(requestBody)
+    // Send request
+    const response = await axios.post('api/users/login_user.php', requestBody, requestConfig)
+    if (response.data && response.data.response === 'success'){
+        commit(LOGIN_USER, user);
+    }
+    return response.data
 }
 
 export function logoutUser({commit}, user) {
